df093bc618541ab21e5760b9dea9ff8c31c577f2
Le Dinh Nguyen <ldnguyen2901@gmail.com>
2025-03-04 14:45:19 +0700

    Error Handling: Middleware

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import exitHook from 'async-exit-hook';
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
 import { env } from '~/config/environment';
 import { APIs_V1 } from '~/routes/v1';
+import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware';
 
 const START_SERVER = () => {
   const app = express();
@@ -13,8 +14,10 @@ const START_SERVER = () => {
   // Use APIs V1
   app.use('/v1', APIs_V1);
 
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware);
+
   app.get('/', (req, res) => {
-    // Test Absolute import mapOrder
     res.send('<h1>Hello World!</h1><hr>');
   });
 
--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,5 +1,6 @@
 import Joi from 'joi';
 import { StatusCodes } from 'http-status-codes';
+import ApiError from '~/utils/ApiError';
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
@@ -13,10 +14,13 @@ const createNew = async (req, res, next) => {
     // Validation thành công, tiếp tục xử lý request tiếp theo.
     next();
   } catch (error) {
-    console.error(error);
-    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      errors: new Error(error).message,
-    });
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
+
+    // res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+    //   errors: new Error(error).message,
+    // });
   }
 };
 
