10940e7319dc650c7e5d5a021cd21ed82401b03d
Le Dinh Nguyen <ldnguyen2901@gmail.com>
2025-03-15 00:32:37 +0700

    Theming

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import { pink } from '@mui/material/colors';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
+import { Typography } from '@mui/material/';
+
 
 function App() {
   return (
     <>
       <div> Nguyenwolf </div>
+
+      <Typography variant='body2' color="text.secondary">Test Typography MUI</Typography>
+
       <Button variant='text'>Text</Button>
       <Button variant='contained'>Contained</Button>
       <Button variant='outlined'>Outlined</Button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <CssBaseline />  {/* Reset the default CSS styles */}
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Reset the default CSS styles */}
+      <App />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
