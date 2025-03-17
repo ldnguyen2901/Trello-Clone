6333c9a0878cdb27aed86803c1177fe8cd836177
Le Dinh Nguyen <ldnguyen2901@gmail.com>
2025-03-17 15:24:13 +0700

    Layout Trello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import Button from '@mui/material/Button';
-import { pink } from '@mui/material/colors';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import HomeIcon from '@mui/icons-material/Home';
-import { Typography } from '@mui/material/';
+import Container from '@mui/material/Container';
 import { useColorScheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 // import useMediaQuery from '@mui/material/';
@@ -16,6 +11,7 @@ import Select from '@mui/material/Select';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+
 function ModeSelect() {
   const { mode, setMode } = useColorScheme();
   const handleChange = (event) => {
@@ -28,9 +24,9 @@ function ModeSelect() {
       <InputLabel id='label-select-dark-light-mode'>Mode</InputLabel>
       <Select labelId='label-select-dark-light-mode' id='select-dark-light-mode' value={mode} label='Mode' onChange={handleChange}>
         <MenuItem value='light'>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <LightModeIcon fontSize='small' /> Light
-          </div>
+          </Box>
         </MenuItem>
         <MenuItem value='dark'>
           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
@@ -38,53 +34,52 @@ function ModeSelect() {
           </Box>
         </MenuItem>
         <MenuItem value='system'>
-          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <SettingsBrightnessIcon fontSize='small' /> System
-          </div>
+          </Box>
         </MenuItem>
       </Select>
     </FormControl>
   );
 }
-function ModeToggle() {
-  const { mode, setMode } = useColorScheme();
-  return (
-    <Button
-      onClick={() => {
-        setMode(mode === 'light' ? 'dark' : 'light');
-      }}
-    >
-      {mode === 'light' ? 'Turn dark' : 'Turn light'}
-    </Button>
-  );
-}
 
 function App() {
   return (
-    <>
-      <ModeSelect />
-      <hr />
-      <ModeToggle />
-      <hr />
-      <div> Nguyenwolf </div>
-
-      <Typography variant='body2' color='text.secondary'>
-        Test Typography MUI
-      </Typography>
-
-      <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
-      <Button variant='outlined'>Outlined</Button>
-      <br />
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <HomeIcon color='primary' />
-      <HomeIcon color='secondary' />
-      <HomeIcon color='success' />
-      <HomeIcon color='action' />
-      <HomeIcon color='disabled' />
-      <HomeIcon sx={{ color: pink[500] }} />
-    </>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor: 'primary.main' }}>
+      <Box
+        sx={{
+          backgroundColor: 'primary.light',
+          width: '100%',
+          height: (theme) => theme.trello.appBarHeight,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <ModeSelect />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.dark',
+          width: '100%',
+          height: (theme) => theme.trello.boardBarHeight,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        Board Bar
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          height: (theme) => `calc(100vh - ${theme.trello.boardBarHeight} - ${theme.trello.appBarHeight})`,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        Board Content
+      </Box>
+    </Container>
   );
 }
 
